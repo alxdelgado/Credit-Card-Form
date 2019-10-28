3162688acdfb30f83119fd3897818624c1fa0238
Alex Delgado <delgadoalex@icloud.com>
2019-10-28 17:35:06 -0500

    card input numbers, name, and expiration date

--- src/components/credit-card-input/credit-card-input.component.jsx
+++ src/components/credit-card-input/credit-card-input.component.jsx
@@ -4,19 +4,37 @@ import './credit-card-input.style.scss';
 
 // The 'credit-card-input' form needs to be a box size housing the 'input-fields' for the CC information. 
 
-const CreditCardInput = (props) => (
-    <div className='credit-card-input_container'>
-        <form>
-            <label className='card-number-input'>
-                Card Number:
-                <input type='number' />
-            </label>
-            <label className='card-name-input'>
-                Card Name:
-                <input type='text' />
-            </label>
-        </form>
-    </div>
-); 
+class CreditCardInput extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(e) {
+        alert('The value is:' +  this.input.value); 
+        e.preventDefault();
+    }
+
+    render() {
+        return (
+            <div className='credit-card-input_container'>
+                <form onSubmit={this.handleSubmit}>
+                    <label className='card-number-input'>
+                        Card Number:
+                        <input type='number' />
+                    </label>
+                    <label className='card-name-input'>
+                        Card Name:
+                        <input type='text' />
+                    </label>
+                    <label className='expiration-date'>
+                        Expiration Date:
+                        <input type='month' />
+                    </label>
+                </form>
+            </div>
+        )
+    }
+};
 
 export default CreditCardInput;
