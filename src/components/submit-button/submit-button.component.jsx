068312269150053d87634f7fafa76f395e114cab
Alex Delgado <delgadoalex@icloud.com>
2019-10-29 14:17:17 -0500

    converted submitButton to functional component

--- src/components/submit-button/submit-button.component.jsx
+++ src/components/submit-button/submit-button.component.jsx
@@ -2,10 +2,19 @@ import React from 'react';
 
 import './submit-button.style.scss'; 
 
-const SubmitButton = (props) => (
-    <button className='submit-button'>
-        <span className='submit-font'>Submit</span>
-    </button>
-); 
+// Functional Component - going to use hooks for state alteration. 
+function SubmitButton() {
+    const handleClick = () => {
+        console.log('click me')
+    }
+
+    return (
+        <button className='submit-button'>
+            <span className='submit-font' onClick={handleClick}>Submit</span>
+        </button>
+    )
+}; 
 
 export default SubmitButton;
+
+
